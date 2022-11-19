8e823ad7899144e9caf1ac8294031b4e2f3b182e
Lapxi010 <Lapxi010@yandex.ru>
2022-11-19 20:27:07 +0300

    initial commit

--- src/store/utils.js
+++
@@ -1,9 +0,0 @@
-export const prepareData = (items) => {
-    return {
-        entities: items.reduce((acc, category) => {
-            acc[category.id] = category
-            return acc
-        }, {}),
-        ids: items.map(({id}) => id)
-    }
-}
